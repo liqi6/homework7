8c3ccd86032d9b64386ae1d0b5497350ff5f9f87
liqi6 <s1143012@gm.ncue.edu.tw>
2024-12-02 23:43:27 +0800

    Update hw7.py

--- hw7.py
+++ hw7.py
@@ -2,12 +2,12 @@ import streamlit as st
 import leafmap.foliumap as leafmap
 
 m = leafmap.Map(center=[23.5,121], zoom=7, minimap_control=True)
-data = "https://raw.githubusercontent.com/liqi6/homework7/refs/heads/main/C-B0074-001.json"
+data = "https://raw.githubusercontent.com/liqi6/homework7/refs/heads/main/nos_p_08.csv"
 m.add_points_from_xy(
   data,
-  x="StationLongitude",
-  y="StationLatitude",
-  icon_names=["StationName", "status", "CountyName","Location"],
+  x="longitude",
+  y="latitude",
+  icon_names=["county", "recordtype", "stationname","noisetype","address"],
   spin=True,
     )
 m.to_streamlit(height=500)
